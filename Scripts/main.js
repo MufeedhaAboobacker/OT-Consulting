9bf8b76eb8508460f68c6a77fd9043f2806a1a53
MufeedhaAboobacker <mufeedha1601@gmail.com>
2025-05-22 12:27:49 +0530

    first commit

---
+++ Scripts/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    $('.your-carousel').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+          }
+        }
+      ]
+    });
+  });
+  
